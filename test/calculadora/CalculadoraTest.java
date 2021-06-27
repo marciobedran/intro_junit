3bf66e09b0c037877ea118e8b406ce67e4b4633b
Márcio Bedran <marcio_bedran@hotmail.com>
2021-06-27 15:37:20 -0300

    git ignore e calculadora test case

--- test/calculadora/CalculadoraTest.java
+++ test/calculadora/CalculadoraTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 @DisplayName("Classe para teste da calculadora")
 public class CalculadoraTest {
 	
@@ -26,6 +27,18 @@ public class CalculadoraTest {
 	public void testSomaDoisNumeros() {
 		int soma = calc.soma(4, 5);		
 		Assertions.assertEquals(9, soma);		
+	}
+	
+	@Test
+	public void testSubtraiDoisNumeros() {
+		int subtracao = calc.subtracao(8, 5);		
+		Assertions.assertEquals(3, subtracao);		
+	}
+	
+	@Test
+	public void testMultiplicaDoisNumeros() {
+		int multiplicacao = calc.multiplicacao(2, 5);		
+		Assertions.assertEquals(10, multiplicacao);		
 	}
 	
 	@Test
@@ -48,6 +61,42 @@ public class CalculadoraTest {
 	public void testDivisaoPorZeroComAssertThrows() {
 		assertThrows(ArithmeticException.class,
 				() -> calc.divisao(8, 0));
+	}
+	
+	@Test
+	public void testSomatoria() {
+		int somatoria = calc.somatoria(5);		
+		Assertions.assertEquals(15, somatoria);		
+	}
+	
+	@Test
+	public void testEhPositivoFalso() {
+		int ehPositivo = calc.ehPositivo(5);		
+		Assertions.assertEquals(False, ehPositivo);		
+	}
+	
+	@Test
+	public void testEhPositivoVerdadeiro() {
+		int ehPositivo = calc.ehPositivo(4);		
+		Assertions.assertEquals(True, ehPositivo);		
+	}
+	
+	@Test
+	public void testComparaIguais() {
+		int compara = calc.compara(4, 4);		
+		Assertions.assertEquals(0, compara);		
+	}
+	
+	@Test
+	public void testComparaMaior() {
+		int compara = calc.compara(4, 3);		
+		Assertions.assertEquals(1, compara);		
+	}
+	
+	@Test
+	public void testComparaMenor() {
+		int compara = calc.compara(4, 5);		
+		Assertions.assertEquals(-1, compara);		
 	}
 
 }
